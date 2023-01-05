d68814e09d4417047968326e7982f5da93118ffd
PatelAbhay550 <90134639+PatelAbhay550@users.noreply.github.com>
2023-01-05 14:33:18 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,36 +1,33 @@
 <?php
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
 
-$mailheader = "From:".$name."<".$email.">\r\n";
+require "vendor/autoload.php";
 
-$recipient = "patelabhay550@gmail.com";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 
-mail($recipient, $subject, $message, $mailheader) or die("Error!");
+$mail = new PHPMailer(true);
 
-echo'
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact form</title>
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="container">
-        <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
-        <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
-        
-    </div>
-</body>
-</html>
-';
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
 
+$mail->isSMTP();
+$mail->SMTPAuth = true;
 
-?>
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "patelabhay550@gmail.com";
+$mail->Password = "ramveerpatel";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("pateljiabhay4@gmail.com", "Abhay");
+
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+$mail->send();
